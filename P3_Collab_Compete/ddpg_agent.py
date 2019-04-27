e30e3702cb53a99f0a6b95fcc4459137d3ab15c2
Shiv Kumar <championballer08@gmail.com>
2019-04-27 18:38:38 +0530

    Finished Project Implementation last phase using DDPG

--- P3_Collab_Compete/ddpg_agent.py
+++ P3_Collab_Compete/ddpg_agent.py
@@ -12,9 +12,9 @@ import torch.optim as optim
 BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-4        # learning rate of the critic
+TAU = 0.2             # for soft update of target parameters
+LR_ACTOR = 1e-3       # learning rate of the actor 
+LR_CRITIC = 1e-3        # learning rate of the critic
 WEIGHT_DECAY = 0        # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -123,7 +123,7 @@ class Agent():
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         self.critic_optimizer.step()
-        torch.nn.utils.clip_grad_norm(self.critic_local.parameters(),1)
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(),1)
         # ---------------------------- update actor ---------------------------- #
         # Compute actor loss
         actions_pred = self.actor_local(states)
